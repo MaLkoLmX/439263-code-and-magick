8dcd32bd4f3f99703695d5c0154333cf4c8a0c65
MaLkoLmX <kadirovfamily@rambler.ru>
2017-11-29 00:44:24 +0400

    Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -6,12 +6,13 @@ var userDialog = document.querySelector('.setup'); // Объявили  пере
 userDialog.classList.remove('hidden'); // Удалили класс .hidden у блока с настройками
 var similarListElement = document.querySelector('.setup-similar-list'); // шаблон
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item'); // Элемент в которым вставляем персонажа
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+var fireballColor = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 function renderWizard() { // функция для случайного определения параметров и копирования персонажа
   var firstName = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристов', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var surname = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
-  var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
   var wizardElement = similarWizardTemplate.cloneNode(true); // объявили глубокое копирование шаблона
 
   wizardElement.querySelector('.setup-similar-label').textContent = firstName[getRandom(firstName)] + surname[getRandom(surname)]; // копируем и добавляем значение к определенному классу
@@ -30,3 +31,62 @@ function wizardCount(wizards) {
 
 similarListElement.appendChild(wizardCount(4)); // добавили в div-список с персонажами
 userDialog.querySelector('.setup-similar').classList.remove('hidden'); // удалили класс
+
+// Кнопки
+var esc = 27;
+var enter = 13;
+// Элементы с которыми будем работать
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+//
+function onPopupEscPress(evt) { // удалить с помошью ESC
+  if (evt.keyCode === esc) {
+    closePopup();
+  }
+}
+// Удаляем класс .hidden. функция удаления класса
+function openPopup() {
+  setup.classList.remove('hidden'); // удаляем класс
+  document.addEventListener('keydown', onPopupEscPress); // при нажатии на кнопку
+}
+// добавления класса
+function closePopup() {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+}
+// при клике удаляем класс .hidden
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+// при нажатии enter удаляем класс .hidden
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === enter) {
+    openPopup();
+  }
+});
+// при клике добавляем класс
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+// добавляем класс при нажатии enter
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === enter) {
+    closePopup();
+  }
+});
+// обявили элементы с которыми будем работать
+var setupWizard = setup.querySelector('.setup-wizard');
+var wizardCoat = setupWizard.querySelector('.wizard-coat');
+var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+var wizardFireball = setup.querySelector('.setup-fireball-wrap');
+
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = coatColor[getRandom(coatColor)];
+});
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = eyesColor[getRandom(eyesColor)];
+});
+wizardFireball.addEventListener('click', function () {
+  wizardFireball.style.background = fireballColor[getRandom(fireballColor)];
+});
